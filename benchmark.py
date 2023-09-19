39f4afc6a685b3e313d80e2478c49f00f40864cc
EthanSMorse <emorse@brooklynsteamcenter.org>
2023-09-19 09:50:37 -0400

    Exercises

---
+++ benchmark.py
@@ -0,0 +1,41 @@
+# Exercise 1: Display your name in the console.
+
+print ("Ethan")
+
+# Exercise 2: Create a variable called "favorite show" and set it equal to your favorite show
+
+favorite_show = ("Family Guy")
+
+#Exercise 3: Create 2 variables called "height" and "age", then display in the console the product of them.
+
+height = ("5'4")
+age = (17)
+
+# Exercise 4: Display "im going to break" in the console
+
+print ("im going to break")
+
+# Exercise 5: Make a list of your 3 favorite foods
+
+fav_foods = ["Chefboyardi", "Mac n Cheese", "Cheez-It"]
+print (fav_foods)
+
+# Exercise 6: Create a conditional statement that checks if your age is greater than or equal to 21, if true, the display "you can rent a car", otherwise display "your beat"
+
+if (age >= 21):
+    print ("You can rent a car")
+else:
+    print ("You're beat")
+
+# Exercise 7: Ask the user for their name, then display in the console "Hello <name>"
+
+
+
+# Exercise 8: Create a loop that will display each item from favorite_foods on a seperate line in the console
+
+
+
+# Exercise 9: Create a while loop that will never run
+
+
+
