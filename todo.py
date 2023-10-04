e1146ddd3663fe43077cc848f8d08c0da7f2c0c3
EthanSMorse <emorse@brooklynsteamcenter.org>
2023-10-04 10:03:28 -0400

    todo2

--- todo.py
+++ todo.py
@@ -7,19 +7,27 @@ while (1):
 # If the user inputs 'add', then it will ask you what item you would like to add.
 # Whatever they type in response will be put into the todo list.
     if (app == "add" or app == "Add"):
+        print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
         added = input("What would you like to add? ")
         todo.append (added)
         print(todo)
+        print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 # If the user inputs 'remove', then it will ask for the number of the item they would like to remove.
 # Whatever number they type in will be subtracted by 1, because list indexes start at 0.
     elif (app == "remove" or app == "Remove"):
+        print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
         remove = int(input("Which numbered item do you want to remove? "))
         del todo [remove - 1]
         print(todo)
+        print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 # if the user inputs 'check',  the current list will be brought up.
     elif (app == "check" or app == "Check"):
+        print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
         print(todo)
+        print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 # Any answers other than this one will be seen as invalid.
     else:
+        print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
         print("The command you input is invalid. Please try again.")
+        print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
     
