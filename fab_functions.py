509a388910c65a87e4713a02d66f6365be32ad32
EthanSMorse <emorse@brooklynsteamcenter.org>
2023-10-17 10:52:20 -0400

    Fab

--- fab_functions.py
+++ fab_functions.py
@@ -1,34 +1,23 @@
-"""
-Task 1: Calculate the Square of a Number
-Write a function that takes an integer as an argument and returns its square.
-"""
+# Task 1: Greeting Function
+# Write a function `greet(name)` that takes a name as a parameter and prints a greeting message like "Hello, [name]!".
 
 
-def square (a1):
-    return a1 ** 2
-print (square (8))
-
-"""
-Task 2: Calculate the Area of a Rectangle:
-Write a function that takes the length and width of a rectangle and returns its area.
-"""
-
-def area (length, width):
-    return length * width
-print (area (64, 248))
-
-"""
-Task 3: Convert Temperature from Celsius to Fahrenheit:
-Write a function that takes a temperature in Celsius and returns 
-the equivalent temperature in Fahrenheit using the correct formula
-"""
+def greet(name):
+    return("Hello, " + name)
+greet("Ethan")
 
 
-def fahrenheit (celsius):
-    return (celsius + 30)
+# Task 2: Sum of Two Numbers
+# Write a function `sum_numbers(a, b)` that takes two numbers as parameters and returns their sum.
 
-"""
-Task 4: Calculate the Average of Numbers:
-Write a function that takes a list of numbers 
-and returns the average (mean) of those numbers.
-"""
+
+# Task 3: Calculate Factorial
+# Write a function `factorial(n)` that calculates the factorial of a given number `n`.
+
+
+# Task 4: Check Even or Odd
+# Write a function `is_even(num)` that takes a number as a parameter and returns `True` if the number is even, and `False` otherwise.
+
+
+# Task 5: Calculate Area of a Rectangle
+# Write a function `calculate_area(length, width)` that calculates and returns the area of a rectangle given its length and width.
